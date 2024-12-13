b727ff346cfdfebe90900a7391aa4cd165c527ae
Eyerusalem Tesfaye <tesfayerusalem@gmail.com>
2024-12-13 15:47:52 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductItem } from '../shared/models/ProductItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  items = [
+    new ProductItem(0, 'Samsung Galaxy A23', 25000, 1, 'a new brand samsung galaxy phone imported from US'),
+    new ProductItem(1, 'Samsung Galaxy A54', 30000, 1, 'a new brand samsung galaxy phone imported from US'),
+    new ProductItem(2, 'Samsung Galaxy A72', 34000, 1, 'a new brand samsung galaxy phone imported from US'),
+    new ProductItem(3, 'Samsung Galaxy A15 5G', 18000, 1, 'a new brand samsung galaxy phone imported from US'),
+    new ProductItem(4, 'Samsung Galaxy A15 5G', 18000, 1, 'a new brand samsung galaxy phone imported from US')
+  ];
   title = 'e-commerce-app';
 }
---
+++ src/shared/models/ProductItem.ts
@@ -0,0 +1,10 @@
+export class ProductItem
+{
+    constructor(
+        public id: number,
+        public name: string,
+        public price: number,
+        public amount: number,
+        public description: string
+    ){}
+}
